722e0a39b3b326ebd5c625b4863b5b841f58e9b8
Seabstian Hufel <sebastian.hufeld@gmail.com>
2025-01-22 12:55:45 +0100

    Aktueller Stand Abschlussprojekt // Aufgabe 4

--- RPG/Classes/Archer.swift
+++ RPG/Classes/Archer.swift
@@ -9,10 +9,12 @@ import Foundation
 
 class Archer: Hero{
     var arrows: Int
+    var backPack: BackPack
     private var sneakAttackCount: Int = 2
     
-    init(name: String, hp: Double, attackPoints: Int, defensePoints: Int ,arrows: Int) {
+    init(name: String, hp: Double, attackPoints: Int, defensePoints: Int ,arrows: Int, backPack: BackPack) {
         self.arrows = arrows
+        self.backPack = backPack
         super.init(name: name, hp: hp, attackPoints: attackPoints, defensePoints: defensePoints)
     }
     
@@ -25,16 +27,48 @@ class Archer: Hero{
         sneakAttackCount -= 1
         let damage = attackPoints * 2
         target.hp -= Double(damage)
-        print("🥷 🏹")
+        print("🥷 🏹🥷 🏹🥷 🏹")
         print("Dein Schleichangriff war erfolgreich! Der Schaden beträgt \(damage). Damit hat \(target.name) noch \(target.hp) Lebenspunkte.")
         print("Du kannst noch \(sneakAttackCount) Schleichangriff(e) durchführen!")
     }
     
     
     func trippleAttack(target: Enemy){
-        if arrows >= 3 {
-            print("Du hast keine Pfeile mehr! Schau im Rucksack nach, ob dort noch welche sind.")
-            return
+        print("Welche Pfeile möchtest du für deinen Angriff verwenden?")
+        print("1. Normale Pfeile (30% mehr Schaden. Aktuell vorhandene Pfeile im eigenen Inventar: \(arrows), im Rucksack \(backPack.arrows) 🏹🏹🏹🏹")
+        print("2. Feuerpfeile (50% mehr Schaden, aber limitierte Stückzahl. Aktuell vorhandene Pfeile: \(backPack.fireArrows) 🔥🔥🔥🔥")
+        
+        if let arrowChoise = Int(readLine()!) {
+            switch arrowChoise {
+            case 1:
+                if arrows >= 3 {
+                    let damage = Int(Double(attackPoints*3) * 1.3)
+                    arrows -= 3
+                    target.hp -= Double(damage)
+                    print("🏹🏹🏹🏹🏹🏹🏹")
+                    print("Du hast \(damage) Schaden mit 3 normalen Pfeilen gemacht. Du hast noch \(arrows) normale Pfeile im Inventar.")
+                } else if backPack.arrows >= 3 {
+                    let damage = Int(Double(attackPoints*3) * 1.3)
+                    backPack.arrows -= 3
+                    target.hp -= Double(damage)
+                    print("🏹🏹🏹🏹🏹🏹🏹")
+                    print("Du hast \(damage) Schaden mit 3 normalen Pfeilen gemacht. Du hast noch \(backPack.arrows) normale Pfeile im Rucksack.")
+                } else {
+                    print("Du hast keine normalen Pfeile mehr! Nutze Feuerpfeile.")
+                }
+            case 2:
+                if backPack.fireArrows >= 3{
+                    let damage = Int(Double(attackPoints*3) * 1.5)
+                    backPack.fireArrows -= 3
+                    target.hp -= Double(damage)
+                    print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
+                    print("Du hast \(damage) Schaden mit 3 Feuerpfeilen gemacht. Du hast noch \(backPack.fireArrows) Feuerpfeile im Rucksack.")
+                } else {
+                    print("Du hast keine Feuerpfeile mehr.")
+                }
+            default:
+                print("Du hast keine Auswahl getroffen, bitte versuche es erneut.")
+            }
         }
     }
 }
